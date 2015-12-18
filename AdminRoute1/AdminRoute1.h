969fcf2f6f63ad16a6a5ebe562737e9edaad2c98
Mike Adkins <michael.h.adkins@gmail.com>
2015-12-18 10:17:39 -0700

    First Commit

---
+++ AdminRoute1/AdminRoute1.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _AdminRoute1_H_
+#define _AdminRoute1_H_
+#include "Arduino.h"
+//add your includes for the project AdminRoute1 here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project AdminRoute1 here
+
+
+
+
+//Do not add code below this line
+#endif /* _AdminRoute1_H_ */
