2571fbfc358fff8caf0510a0b43288f8cf02a096
Mike Adkins <michael.h.adkins@gmail.com>
2016-04-03 14:22:46 -0600

    Adding Neighbor updates

--- libraries/AODV/Neighbor.cpp
+++ libraries/AODV/Neighbor.cpp
@@ -3,62 +3,16 @@ const uint8_t MOVING_AVERAGE_SIZE = 5;
 Neighbor::Neighbor() {
 
 	address = XBeeAddress64();
-	nextHop = XBeeAddress64();
+	downStreamNeighbor = XBeeAddress64();
 	dataRate = 0;
-	seqNum = 0;
-	qualityOfPath = 0;
-	neighborhoodCapacity = 0;
-	routeFlag = false;
-	sinkAddress = XBeeAddress64();
-	relativeDistance = 0;
-	packetLoss = 0;
-	relativeDistanceAvg = 0;
-	rssi = 0;
-	previousTimeStamp = 0;
-	timeStamp = 0;
-	timeoutLength = 0;
-	previousRelativeDistance = 0;
 
 }
 
-Neighbor::Neighbor(const XBeeAddress64& address, const XBeeAddress64& nextHop, float neighborDataRate, uint8_t seqNum,
-		float qualityOfPath, float neighborhoodCapacity, bool routeFlag, const XBeeAddress64& sinkAddress,
-		double relativeDistance, double rssi, unsigned long timeoutLength) {
+Neighbor::Neighbor(const float dataRate, const XBeeAddress64& address, const XBeeAddress64& downStreamNeighbor) {
 
 	this->address = address;
-	this->nextHop = nextHop;
+	this->downStreamNeighbor = downStreamNeighbor;
 	this->dataRate = dataRate;
-	this->seqNum = seqNum;
-	this->qualityOfPath = qualityOfPath;
-	this->neighborhoodCapacity = neighborhoodCapacity;
-	this->routeFlag = routeFlag;
-	this->sinkAddress = sinkAddress;
-	this->rssi = rssi;
-	this->packetLoss = 0;
-	addToRelativeDistance(relativeDistance);
-	timeStamp = millis();
-	previousTimeStamp = timeStamp;
-	this->timeoutLength = timeoutLength;
-	previousRelativeDistance = 0;
-	relativeDistanceAvg = 0;
-}
-
-void Neighbor::addToRelativeDistance(const double relativeDistance) {
-	if (relativeDistanceQueue.size() < MOVING_AVERAGE_SIZE) {
-		relativeDistanceQueue.push_front(relativeDistance);
-	} else {
-		previousRelativeDistance = relativeDistanceQueue.back();
-		relativeDistanceQueue.pop_back();
-
-		double total = 0;
-		for (int i = 0; i < MOVING_AVERAGE_SIZE; ++i) {
-			total += relativeDistanceQueue[i];
-		}
-
-		relativeDistanceAvg = total / MOVING_AVERAGE_SIZE;
-
-	}
-
 }
 
 const XBeeAddress64& Neighbor::getAddress() const {
@@ -77,112 +31,10 @@ void Neighbor::setDataRate(float dataRate) {
 	this->dataRate = dataRate;
 }
 
-float Neighbor::getNeighborhoodCapacity() const {
-	return neighborhoodCapacity;
+const XBeeAddress64& Neighbor::getDownStreamNeighbor() const {
+	return downStreamNeighbor;
 }
 
-void Neighbor::setNeighborhoodCapacity(float neighborhoodCapacity) {
-	this->neighborhoodCapacity = neighborhoodCapacity;
-}
-
-float Neighbor::getPacketLoss() const {
-	return packetLoss;
-}
-
-void Neighbor::setPacketLoss(float packetLoss) {
-	this->packetLoss = packetLoss;
-}
-
-uint8_t Neighbor::getQualityOfPath() const {
-	return qualityOfPath;
-}
-
-void Neighbor::setQualityOfPath(uint8_t qualityOfPath) {
-	this->qualityOfPath = qualityOfPath;
-}
-
-bool Neighbor::isRouteFlag() const {
-	return routeFlag;
-}
-
-void Neighbor::setRouteFlag(bool routeFlag) {
-	this->routeFlag = routeFlag;
-}
-
-uint8_t Neighbor::getSeqNum() const {
-	return seqNum;
-}
-
-void Neighbor::setSeqNum(uint8_t seqNum) {
-	this->seqNum = seqNum;
-}
-
-const XBeeAddress64& Neighbor::getSinkAddress() const {
-	return sinkAddress;
-}
-
-void Neighbor::setSinkAddress(const XBeeAddress64& sinkAddress) {
-	this->sinkAddress = sinkAddress;
-}
-
-void Neighbor::updateTimeStamp() {
-	previousTimeStamp = timeStamp;
-	timeStamp = millis();
-}
-
-bool Neighbor::timerExpired() {
-	if (millis() - timeStamp > timeoutLength) {
-		return true;
-	}
-	return false;
-}
-
-bool Neighbor::compare(const Neighbor &b) {
-	return address.equals(b.address);
-}
-
-unsigned long Neighbor::getPreviousTimeStamp() const {
-	return previousTimeStamp;
-}
-
-unsigned long Neighbor::getTimeStamp() const {
-	return timeStamp;
-}
-
-bool Neighbor::equals(const Neighbor& neighbor) const {
-	return address.equals(neighbor.getAddress());
-}
-
-double Neighbor::getPreviousRelativeDistance() const {
-	return previousRelativeDistance;
-}
-
-double Neighbor::getRelativeDistanceAvg() const {
-	return relativeDistanceAvg;
-}
-
-double Neighbor::getRssi() const {
-	return rssi;
-}
-
-void Neighbor::setRssi(double rssi) {
-	this->rssi = rssi;
-}
-
-void Neighbor::printNeighbor() const {
-	Serial.print('<');
-	address.printAddress(&Serial);
-	Serial.print(',');
-	Serial.print(dataRate);
-	Serial.print(',');
-	Serial.print(neighborhoodCapacity);
-	Serial.print('>');
-}
-
-const XBeeAddress64& Neighbor::getNextHop() const {
-	return nextHop;
-}
-
-void Neighbor::setNextHop(const XBeeAddress64& nextHop) {
-	this->nextHop = nextHop;
+void Neighbor::setDownStreamNeighbor(const XBeeAddress64& downStreamNeighbor) {
+	this->downStreamNeighbor = downStreamNeighbor;
 }
--- libraries/AODV/Neighbor.h
+++ libraries/AODV/Neighbor.h
@@ -17,59 +17,18 @@ class Neighbor {
 
 	private:
 		XBeeAddress64 address;
-		XBeeAddress64 sinkAddress;
-		XBeeAddress64 nextHop;
-		double relativeDistance;
-		double previousRelativeDistance;
+		XBeeAddress64 downStreamNeighbor;
 		float dataRate;
-		float neighborhoodCapacity;
-		float packetLoss;
-		uint8_t seqNum;
-		uint8_t qualityOfPath;
-		bool routeFlag;
-		deque<double> relativeDistanceQueue;
-		double rssi;
-		double relativeDistanceAvg;
-		unsigned long previousTimeStamp;
-		unsigned long timeStamp;
-		unsigned long timeoutLength;
 
 	public:
 		Neighbor();
-		Neighbor(const XBeeAddress64& address, const XBeeAddress64& nextHop, float neighborDataRate, uint8_t seqNum,
-				float qualityOfPath, float neighborhoodCapacity, bool routeFlag, const XBeeAddress64& sinkAddress,
-				double relativeDistance, double rssi, unsigned long timeoutLength);
-
+		Neighbor(const float dataRate, const XBeeAddress64& address, const XBeeAddress64& downStreamNeighbor);
 		const XBeeAddress64& getAddress() const;
 		void setAddress(const XBeeAddress64& address);
 		float getDataRate() const;
 		void setDataRate(float dataRate);
-		float getNeighborhoodCapacity() const;
-		void setNeighborhoodCapacity(float neighborhoodCapacity);
-		float getPacketLoss() const;
-		void setPacketLoss(float packetLoss);
-		uint8_t getQualityOfPath() const;
-		void setQualityOfPath(uint8_t qualityOfPath);
-		bool isRouteFlag() const;
-		void setRouteFlag(bool routeFlag);
-		uint8_t getSeqNum() const;
-		void setSeqNum(uint8_t seqNum);
-		const XBeeAddress64& getSinkAddress() const;
-		void setSinkAddress(const XBeeAddress64& sinkAddress);
-		void updateTimeStamp();
-		bool timerExpired();
-		bool compare(const Neighbor &b);
-		void printNeighbor() const;
-		void addToRelativeDistance(const double relative);
-		unsigned long getTimeStamp() const;
-		unsigned long getPreviousTimeStamp() const;
-		bool equals(const Neighbor& neighbor) const;
-		double getPreviousRelativeDistance() const;
-		double getRelativeDistanceAvg() const;
-		double getRssi() const;
-		void setRssi(double rssi);
-		const XBeeAddress64& getNextHop() const;
-		void setNextHop(const XBeeAddress64& nextHop);
+		const XBeeAddress64& getDownStreamNeighbor() const;
+		void setDownStreamNeighbor(const XBeeAddress64& downStreamNeighbor);
 };
 
 #endif
