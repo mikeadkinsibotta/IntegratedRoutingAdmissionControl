2d54d1c20645973a164f57cd8130294f0622fbfd
Mike Adkins <michael.h.adkins@gmail.com>
2016-08-21 11:20:49 -0600

    updated data rate

--- libraries/ADPCM/pcadpcm.cpp
+++ libraries/ADPCM/pcadpcm.cpp
@@ -1,5 +1,6 @@
 #include "pcadpcm.h"
 #define DELAY 260
+#define DELAY3 400
 
 /*****************************************************************************
  *	ADPCMEncoder - ADPCM encoder routine                                     *
@@ -17,7 +18,7 @@ ADPCM::ADPCM() {
 }
 
 void ADPCM::fiveBitADPCMEncoderGetter(uint8_t code, int i, uint8_t buffer[]) {
-	switch(i) {
+	switch (i) {
 
 		case 0:
 			buffer[0] = code << 3;
@@ -53,7 +54,7 @@ void ADPCM::fiveBitADPCMEncoderGetter(uint8_t code, int i, uint8_t buffer[]) {
 
 void ADPCM::threeBitADPCMEncoderGetter(uint8_t code, int i, uint8_t buffer[]) {
 
-	switch(i) {
+	switch (i) {
 
 		case 0:
 			buffer[0] = code << 5;
@@ -103,8 +104,8 @@ char ADPCM::ADPCMEncoder(short sample, int bits) {
 
 	diff = sample - predsample;
 
-	if(diff < 0) {
-		switch(bits) {
+	if (diff < 0) {
+		switch (bits) {
 			case 5:
 				code = 16;
 				break;
@@ -125,8 +126,8 @@ char ADPCM::ADPCMEncoder(short sample, int bits) {
 
 	/* Quantize the difference into the 4-bit ADPCM code using the the quantizer step size */
 
-	if(diff >= step) {
-		switch(bits) {
+	if (diff >= step) {
+		switch (bits) {
 			case 5:
 				code |= 8;
 				break;
@@ -143,8 +144,8 @@ char ADPCM::ADPCMEncoder(short sample, int bits) {
 
 		diff -= step;
 	}
-	if(diff >= (step >> 1)) {
-		switch(bits) {
+	if (diff >= (step >> 1)) {
+		switch (bits) {
 			case 5:
 				code |= 4;
 				diff -= (step >> 1);
@@ -159,8 +160,8 @@ char ADPCM::ADPCMEncoder(short sample, int bits) {
 		}
 	}
 
-	if(diff >= (step >> 2)) {
-		switch(bits) {
+	if (diff >= (step >> 2)) {
+		switch (bits) {
 			case 5:
 				code |= 2;
 				diff -= (step >> 2);
@@ -170,35 +171,35 @@ char ADPCM::ADPCMEncoder(short sample, int bits) {
 		}
 	}
 
-	if(bits == 5 && diff >= (step >> 3))
+	if (bits == 5 && diff >= (step >> 3))
 		code |= 1;
 
 	/* Inverse quantize the ADPCM code into a predicted difference using the quantizer step size */
 	diff = 0;
 
-	if(((bits == 4) && (code & 4)) || ((bits == 3) && (code & 2)) || ((bits == 5) && (code & 8))
+	if (((bits == 4) && (code & 4)) || ((bits == 3) && (code & 2)) || ((bits == 5) && (code & 8))
 			|| ((bits == 2) && (code & 1)))
 		diff += step;
 
-	if(((bits == 4) && (code & 2)) || ((bits == 3) && (code & 1)) || ((bits == 5) && (code & 4)))
+	if (((bits == 4) && (code & 2)) || ((bits == 3) && (code & 1)) || ((bits == 5) && (code & 4)))
 		diff += (step >> 1);
 
-	if(((bits == 4) && (code & 1)) || ((bits == 5) && (code & 2)))
+	if (((bits == 4) && (code & 1)) || ((bits == 5) && (code & 2)))
 		diff += (step >> 2);
 
-	if((bits == 5) && (code & 1))
+	if ((bits == 5) && (code & 1))
 		diff += (step >> 3);
 
-	if(bits == 4)
+	if (bits == 4)
 		diff += (step >> 3);
-	else if(bits == 2)
+	else if (bits == 2)
 		diff += (step >> 1);
-	else if(bits == 3)
+	else if (bits == 3)
 		diff += (step >> 2);
 	else
 		diff += (step >> 4);
 
-	if((bits == 4 && (code & 8)) || (bits == 3 && (code & 4)) || ((bits == 5) && (code & 16))
+	if ((bits == 4 && (code & 8)) || (bits == 3 && (code & 4)) || ((bits == 5) && (code & 16))
 			|| (bits == 2 && (code & 2)))
 		predsample -= diff;
 	else
@@ -207,36 +208,36 @@ char ADPCM::ADPCMEncoder(short sample, int bits) {
 	/* Fixed predictor computes new predicted sample by adding the old predicted sample to predicted difference */
 
 	/* Check for overflow of the new predicted sample */
-	if(predsample > 32767)
+	if (predsample > 32767)
 		predsample = 32767;
-	else if(predsample < -32767)
+	else if (predsample < -32767)
 		predsample = -32767;
 
 	/* Find new quantizer stepsize index by adding the old index to a table lookup using the ADPCM code */
-	if(bits == 4)
+	if (bits == 4)
 		index += IndexTable[code];
-	else if(bits == 3)
+	else if (bits == 3)
 		index += IndexTable3bit[code];
-	else if(bits == 2)
+	else if (bits == 2)
 		index += IndexTable2Bit[code];
 	else
 		index += IndexTable5Bit[code];
 
 	/* Check for overflow of the new quantizer step size index */
-	if(index < 0)
+	if (index < 0)
 		index = 0;
-	if(index > 88)
+	if (index > 88)
 		index = 88;
 
 	/* Save the predicted sample and quantizer step size index for next iteration */
 	state.prevsample = (short) predsample;
 	state.previndex = index;
 
-	if(bits == 4)
+	if (bits == 4)
 		code &= 0x0f;
-	else if(bits == 3)
+	else if (bits == 3)
 		code &= 0x07;
-	else if(bits == 2)
+	else if (bits == 2)
 		code &= 0x03;
 	else
 		code &= 0x1f;
@@ -270,54 +271,54 @@ int ADPCM::ADPCMDecoder(char code, int bits) {
 	/* Inverse quantize the ADPCM code into a difference using the quantizer step size */
 
 	diff = 0;
-	if(((bits == 4) && (code & 4)) || ((bits == 3) && (code & 2)) || ((bits == 5) && (code & 8))
+	if (((bits == 4) && (code & 4)) || ((bits == 3) && (code & 2)) || ((bits == 5) && (code & 8))
 			|| ((bits == 2) && (code & 1)))
 		diff += step;
 
-	if(((bits == 4) && (code & 2)) || ((bits == 3) && (code & 1)) || ((bits == 5) && (code & 4)))
+	if (((bits == 4) && (code & 2)) || ((bits == 3) && (code & 1)) || ((bits == 5) && (code & 4)))
 		diff += (step >> 1);
 
-	if(((bits == 4) && (code & 1)) || ((bits == 5) && (code & 2)))
+	if (((bits == 4) && (code & 1)) || ((bits == 5) && (code & 2)))
 		diff += (step >> 2);
 
-	if((bits == 5) && (code & 1))
+	if ((bits == 5) && (code & 1))
 		diff += (step >> 3);
 
-	if(bits == 4)
+	if (bits == 4)
 		diff += (step >> 3);
-	else if(bits == 2)
+	else if (bits == 2)
 		diff += (step >> 1);
-	else if(bits == 3)
+	else if (bits == 3)
 		diff += (step >> 2);
 	else
 		diff += (step >> 4);
 
-	if((bits == 4 && (code & 8)) || ((bits == 3) && (code & 4)) || ((bits == 5) && (code & 16))
+	if ((bits == 4 && (code & 8)) || ((bits == 3) && (code & 4)) || ((bits == 5) && (code & 16))
 			|| ((bits == 2) && (code & 2)))
 		predsample -= diff;
 	else
 		predsample += diff;
 
 	/* Check for overflow of the new predicted sample */
-	if(predsample > 32767)
+	if (predsample > 32767)
 		predsample = 32767;
-	else if(predsample < -32768)
+	else if (predsample < -32768)
 		predsample = -32768;
 
 	/* Find new quantizer step size by adding the old index and a table lookup using the ADPCM code */
-	if(bits == 4)
+	if (bits == 4)
 		index += IndexTable[code];
-	else if(bits == 3)
+	else if (bits == 3)
 		index += IndexTable3bit[code];
-	else if(bits == 2)
+	else if (bits == 2)
 		index += IndexTable2Bit[code];
 	else
 		index += IndexTable5Bit[code];
 
 	/* Check for overflow of the new quantizer step size index */
-	if(index < 0)
+	if (index < 0)
 		index = 0;
-	if(index > 88)
+	if (index > 88)
 		index = 88;
 
 	/* Save predicted sample and quantizer step size index for next iteration*/
@@ -330,7 +331,7 @@ int ADPCM::ADPCMDecoder(char code, int bits) {
 
 void ADPCM::fiveBitEncode(uint8_t buffer[]) {
 	uint16_t soundData = 0;
-	for(int n = 0; n < 8; n++) {
+	for (int n = 0; n < 8; n++) {
 
 		delayMicroseconds(DELAY);
 		//make sample
@@ -368,9 +369,9 @@ uint8_t ADPCM::fourBitEncode() {
 void ADPCM::threeBitEncode(uint8_t buffer[]) {
 	uint16_t soundData = 0;
 
-	for(int f = 0; f < 8; f++) {
+	for (int f = 0; f < 8; f++) {
 
-		delayMicroseconds(DELAY);
+		delayMicroseconds(DELAY3);
 		//make sample
 		uint16_t sample = (soundData << 8) & 0xff00;
 		sample |= soundData;
